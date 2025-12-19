175b2debc33585813e0dcbb4833b986250ddf604
lobsangshakya <lobsangshakya5@gmail.com>
2025-12-19 20:55:42 +0530

    updation on action.py

--- dot_catcher/backend/action_handler.py
+++ dot_catcher/backend/action_handler.py
@@ -28,7 +28,11 @@ def catch_dot():
 
 if __name__ == '__main__':
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     app.run(host='0.0.0.0', port=5002, debug=True)
+=======
+    app.run(host='0.0.0.0', port=5002, debug=True)
+>>>>>>> Stashed changes
 =======
     app.run(host='0.0.0.0', port=5002, debug=True)
 >>>>>>> Stashed changes
